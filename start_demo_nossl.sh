0e3de1cf31c68f6d92c16f26889ccd646900aa3a
fp <fp@wemove.com>
2025-07-01 15:40:00 +0200

    Update start script

---
+++ start_demo_nossl.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+UNAME=$(which uname)
+GREP=$(which grep)
+
+printf "Configure sample .env\n"
+cp .env.sample .env
+
+printf "Set Host to ingrid.localhost.\n"
+export HOST=ingrid.localhost
+
+printf "Create database folder.\n"
+
+mkdir -p _data/postgres-db
+
+printf "Set permissions for default user.\n"
+sudo chown -R 1000:1000 ./_data
+
+printf "Copy initial sample webmapclient data.\n"
+cp -r portal/WebmapClientData.test/* _data/portal/WebmapClientData
+
+printf "Increase memory for elastic.\n"
+if [[ $($UNAME -a | $GREP -i linux) ]]; then
+sysctl vm.max_map_count
+sudo sysctl -w vm.max_map_count=262144
+fi
+
+if [[ $($UNAME -a | $GREP -i darwin) ]] then
+printf "Can't run these lines on a mac. Skipping.\n"
+fi
+
+printf "The installation uses docker images from a non-public docker registry.\n You can also build your own images by following the instructions in the specific ingrid repositories.\n"
+sudo docker login -u readonly -p readonly docker-registry.wemove.com &> /dev/null # password "readonly"
+sudo docker compose up -d
+
+printf "First time boot requires around 60 seconds to be ready."
+sleep 60
+sudo docker compose ps
+exit 0
