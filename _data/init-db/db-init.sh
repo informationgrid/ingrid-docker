0b42f3306a4ded8de67dabaaa3a2263ee3452ec4
fp <fp@wemove.com>
2025-08-28 14:43:29 +0200

    Remove obsolete configuration

--- _data/init-db/db-init.sh
+++ _data/init-db/db-init.sh
@@ -4,8 +4,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE keycloak;
     CREATE DATABASE ige;
-	CREATE DATABASE ingrid_portal;
-    CREATE DATABASE mdek;
     CREATE DATABASE harvester;
 EOSQL
 
